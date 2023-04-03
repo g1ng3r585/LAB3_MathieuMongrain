e3278c37edc41fb6ce165d2cfd8f4de271046681
g1ng3r585 <mathieumongrain@icloud.com>
2023-04-03 15:34:30 -0400

    UI
    cours 1

--- Assets/_MyAssets/Scripts/PlateformesTombantes1.cs
+++ Assets/_MyAssets/Scripts/PlateformesTombantes1.cs
@@ -12,8 +12,11 @@ public class PlateformesTombantes1 : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
-        _rb.AddForce(0,1, 0);
+        if (collision.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            _rb.AddForce(0,1, 0);
+        }
     }
 
 
